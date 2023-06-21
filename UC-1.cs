f3da0e1123227c41a2ecf98ee165e6a7592452fd
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-06-21 18:57:11 +0530

    Welcome file

---
+++ UC-1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Snake_and_Ladder
+{
+    public class UC_1
+    {
+        public static void solution()
+        {
+            Console.WriteLine("Welcome to snake ladder game");
+            int start = 0;
+            Console.WriteLine("The begining of game is:" + start);
+        }
+    }
+}
