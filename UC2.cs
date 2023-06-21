eec368b4ee881f6a9d633a65100f652d2d83f49f
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-06-21 19:03:37 +0530

    Position file

---
+++ UC2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Snake_and_Ladder
+{
+    public class UC2
+    {
+        public static void solution()
+        {
+            Random random=new Random(7);
+            int diceoutput = random.Next(7);
+            int position = 0;
+            Console.WriteLine("The diceoutput number:" + diceoutput);
+        }
+    }
+}
